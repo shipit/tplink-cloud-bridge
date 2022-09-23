4b345e3559e3b0186e7d372d21780580cee867eb
Sumeet Parmar <z.sumeet@gmail.com>
2022-09-23 10:18:19 -0700

    first submission

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const respond = function (res, { payload }) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(payload);
+};
+
+app.get("/", (req, res) => {
+  const payload = {
+    message: "take me to your leader",
+  };
+  respond(res, { payload });
+});
+
+app.listen(port, () => {
+  console.log(`Monster bridge started on ${port}`);
+});
