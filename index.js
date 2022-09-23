7a95102109bd535e2ab25950e36881037d29a166
Sumeet Parmar <shipit@users.noreply.github.com>
2022-09-23 11:24:59 -0700

    [Chore] CI (#2)
    * added CI workflow
    
    * added and configured eslint
    
    * added linter to scripts

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const app = express();
-const port = 4000;
+const express = require('express')
+const app = express()
+const port = 4000
 
 const respond = function (res, { payload }) {
-  res.setHeader("Content-Type", "application/json");
-  res.send(payload);
-};
+  res.setHeader('Content-Type', 'application/json')
+  res.send(payload)
+}
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const payload = {
-    message: "take me to your leader",
-    "one-last-thing": "think, different",
-  };
-  respond(res, { payload });
-});
+    message: 'take me to your leader',
+    'one-last-thing': 'think, different',
+  }
+  respond(res, { payload })
+})
 
 app.listen(port, () => {
-  console.log(`Monster bridge started on ${port}`);
-});
+  console.log(`Monster bridge started on ${port}`)
+})
